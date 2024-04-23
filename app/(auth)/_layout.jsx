301dda7ab4133778f4b97c4f1a97905e66f057e3
edmondgaks <edmondgaks@gmail.com>
2024-04-23 08:38:00 +0200

    Correcting some layout

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,11 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { Tabs, Redirect } from 'expo-router';
 
 const AuthLayout = () => {
   return (
-    <>
-    <Tabs>
-        <Tabs.Screen name="home" />
-    </Tabs>
-    </>
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
   )
 }
 
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { Tabs, Redirect } from 'expo-router'
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+    <Tabs>
+        <Tabs.Screen name="home" />
+    </Tabs>
+    </>
   )
 }
 
